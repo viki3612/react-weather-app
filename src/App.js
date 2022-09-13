dc7391162d39a221d32519f7ce8c08e5c5de7e27
viki3612 <viktorianikolova95@gmail.com>
2022-09-13 16:59:54 +0100

    added bootstrap, created weather component and html

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import "./App.css";
+import React from "react";
 import Weather from "./Weather";
+
 export default function App() {
   return (
     <div className="App">
-      <h1> WeatherApp </h1>
-      <Weather />
-      <footer>
-        {" "}
-        This project is coded by{" "}
-        <a
-          href="https://viktoria-nikolova-portfolio.netlify.app"
-          target="_blank "
-          rel="noreferrer"
-        >
+      <div className="container">
+        <Weather />
+        <footer>
           {" "}
-          Viktoria{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/viki3612/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open-sourced on Github
-        </a>
-      </footer>
+          This project is coded by{" "}
+          <a
+            href="https://viktoria-nikolova-portfolio.netlify.app"
+            target="_blank "
+            rel="noreferrer"
+          >
+            {" "}
+            Viktoria{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/viki3612/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,43 @@
 import React, { useState } from "react";
-import axios from "axios";
+
 export default function Weather() {
-  const [city, setCity] = useState("null");
-  const [message, setMessage] = useState("null");
-  const [weather, setWeather] = useState("null");
-
-  function showWeather(response) {
-    setMessage(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description,
-    });
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "cf033e3091dfce784674bd3c6246897a";
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-    axios.get(url).then(showWeather);
-  }
-
-  function handleMessage(event) {
-    setCity(event.target.value);
-  }
-
-  let form = (
-    <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="enter a city" onChange={handleMessage} />
-      <input type="submit" value="Submit" />
-    </form>
-  );
-  if (message) {
-    return (
-      <div>
-        {form}
-
-        <ul className="Message">
-          {" "}
-          <li>Temp: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {Math.round(weather.humidity)}%</li>
-          <li>Wind: {Math.round(weather.wind)}km/h</li>{" "}
-          <li>
-            {" "}
-            <img src={weather.icon} alt="weather icon" />{" "}
-          </li>{" "}
-        </ul>
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Sofia</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="weather-image"
+          />
+          20°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Wind: 20 km/h</li>
+            <li>Precipitation: 15%</li>
+            <li>Humidity:35%</li>
+          </ul>
+        </div>
       </div>
-    );
-  } else {
-    return form;
-  }
+    </div>
+  );
 }
