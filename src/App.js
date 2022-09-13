7d0796bf7419f36d4f234ac4cf206d1f50c838e0
viki3612 <viktorianikolova95@gmail.com>
2022-09-13 15:38:10 +0100

    added links

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">WeatherApp</div>;
+  return (
+    <div className="App">
+      <h1> WeatherApp </h1>
+      <footer>
+        {" "}
+        This project is coded by{" "}
+        <a
+          href="https://viktoria-nikolova-portfolio.netlify.app"
+          target="_blank "
+          rel="noreferrer"
+        >
+          {" "}
+          Viktoria{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/viki3612/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
