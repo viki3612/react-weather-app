dd72421a7e0df8f376316f8ddb35cab205e0df00
viki3612 <viktorianikolova95@gmail.com>
2022-09-14 17:28:28 +0100

    added css to app

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
-
+import React from "react";
+import "./Weather.css";
+import axios from "axios";
 export default function Weather() {
   return (
     <div className="Weather">
@@ -10,14 +11,19 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city..."
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
-      <h1>Sofia</h1>
+      <h1 className="city mb-3">Sofia</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Cloudy</li>
@@ -26,9 +32,10 @@ export default function Weather() {
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="weather-image"
+            alt="weather"
           />
-          20°C
+          <span className="temperature">20</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
