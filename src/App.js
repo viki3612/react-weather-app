affd68ba6d5ce269af5c10082b9540717183514a
viki3612 <viktorianikolova95@gmail.com>
2022-12-14 17:42:53 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Gabrovo" />
+        <Weather defaultCity="Cancun" />
         <footer>
           {" "}
           This project is coded by{" "}
