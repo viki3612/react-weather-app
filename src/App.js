a76f58f3427dcb2aaa189ec920997de019b561ae
viki3612 <viktorianikolova95@gmail.com>
2022-12-14 18:05:21 +0000

    updated css on middle section

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="Cancun" />
+        <Weather defaultCity="Gabrovo" />
         <footer>
           {" "}
           This project is coded by{" "}
